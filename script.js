f98c29b68e027d482d608057aeb5039c58b3dcd7
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2025-03-19 11:58:20 +0000

    Update - 0.1v

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 const btn = document.getElementById("btn");
+
 btn.addEventListener("click", function () {
-    const inputType = document.getElementById("inputType").value;
-    const vaildPopSucess = document.querySelector('.sucess')
-    const notVaildSucess = document.querySelector('.not-sucess')
-    const btnReset = document.getElementById('btnReset')
-    const validAge = 19;
+  const inputType = document.getElementById("inputType").value;
+  const vaildPopSucess = document.querySelector(".sucess");
+  const notVaildSucess = document.querySelector(".not-sucess");
+  const btnReset = document.getElementById("btnReset");
+  const validAge = 18;
 
-    if (inputType == "") {
-        alert("Enter your age");
-    }
+  if (inputType == "") {
+    alert("Enter your age");
+    return;
+  }
 
-    if (validAge <= inputType) {
-        // console.log("You are Eligible")
-        vaildPopSucess.style.display = "block";
-        notVaildSucess.style.display = "none"
-    } else {
-        // console.log("you are NOT Eligible")
-        notVaildSucess.style.display = "block"
-        vaildPopSucess.style.display = "none"
-    }
+  const age = parseInt(inputType)
 
-    if (inputType == "") {
-        vaildPopSucess.style.display = "none"
-        notVaildSucess.style.display = "none"
-    }
+  if (age < 0 || age > 100) {
+    alert("Please enter a valid age between 0 and 100.");
+    return;
+    // console.log("You are Eligible")
+ 
+  }
+  
+  if(age > validAge){
+    vaildPopSucess.style.display = "block";
+    notVaildSucess.style.display = "none";
+  }else {
+    // console.log("you are NOT Eligible")
+    notVaildSucess.style.display = "block";
+    vaildPopSucess.style.display = "none";
+  }
 
-    btnReset.addEventListener('click', () => {
-        location.reload()
-    })
+  if (inputType == "") {
+    vaildPopSucess.style.display = "none";
+    notVaildSucess.style.display = "none";
+  }
 
+  btnReset.addEventListener("click", () => {
+    location.reload();
+  });
 });
